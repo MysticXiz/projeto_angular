c4f0702449fa70d54077ec69c1768ca704587b6b
Danilo <danilovitoriastf@gmail.com>
2024-11-02 17:11:48 -0300

    Adicionado pages: home, catalogo, login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
