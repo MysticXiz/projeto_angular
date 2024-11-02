c46a7897e2d176c3645c82c461a6c10e9f6b43c1
Danilo <danilovitoriastf@gmail.com>
2024-11-02 19:17:22 -0300

    Footer em progresso

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: ' ', component: HomeComponent },
   { path: "catalogo", component: CatalogoComponent },
   { path: 'login', component: LoginComponent}
 ];
